e3a841cc4a218fa5274c896aae387901cc027c86
15b3 <30496251+15b3@users.noreply.github.com>
2023-03-26 21:37:59 +0900

    update README

--- README.md
+++ README.md
@@ -1,9 +1,63 @@
 # gtick
 
+Matplotlibの年月日時の日本語フォーマッター
+
+2,3日程度の期間を「時」ごとに表示する際の利用を想定した日本語表示です。
+表示が「H時(HH)」「M月D日」「Y年」のようになります。
+
+## 特徴
+
+- 「時」は全ての場所で表示される。
+- 最初の場所（``start_pos=0`` ）では、「時」だけではなく、年・月・日が表示される。
+- 年、月、日は、変わったタイミングのみで、表示される。
+- 月や日が0埋めされずに、「3月21日」のように表示される。
+- 「年」は省略可能 (year=False)
+- 「時」は、「06」から「6時」(``ji=True``)に切り替え可能。
+
+です。
+
+## クイックスタート
+
+``matplotlib.dates.HourLocator`` と組み合わせて使うことを想定しています。
+
+```python
+from gtick import JDayHourFormatter
+
+import matplotlib.dates as mdates
+
+# 何かデータをプロット
+# plt.plot(x, y)
+
+# HourLocatorを利用する: この例だと3時間ごとにおく。
+# Locatorを調整することで、軸のラベル間隔などは調整可能。
+axes.xaxis.set_major_locator(mdates.HourLocator(byhour=[i for i in range(0, 24, 3)]))
+# major locatorの場所に、JDayHourFormatterを利用する
+axes.xaxis.set_major_formatter(JDayHourFormatter())
+```
+
+
+## Examples
+
+### A
+
+先頭には、時・月日・年が表示される。日が変わると、日が表示される。
 ![Sample A](png/sample_a.png)
 
+
+### B
+
+「0時」じゃなくても、先頭には、時・月日・年が表示される。
+また、日だけではなく、年や月が変わると、年や月が表示される。
 ![Sample B](png/sample_b.png)
 
+
+### C
+
+先頭と0時が近いと、詰まってしまうことがあるので、その場合は、オプションで、年・月・日の表示をずらすことができる。
 ![Sample C](png/sample_c.png)
 
+
+### D
+
+年は非表示にすることが可能。また、時は、6時の表示もできる。
 ![Sample D](png/sample_d.png)
--- src/gtick.py
+++ src/gtick.py
@@ -1,12 +1,13 @@
+from warnings import warn
 from matplotlib import ticker, dates
 
 
 class JDayHourFormatter(ticker.Formatter):
-    def __init__(self, start_pos=0, ji=False, year=True):
+    def __init__(self, start_pos: int = 0, ji: bool = False, year: bool = True):
         """日本語の年月日時フォーマッター
 
-        最初は、(年)月日時を表示させて、残りは時のみを表示される。
-        年、月、日が変わる場合は、日や月が表示される。
+        先頭は、(年)月日時を表示される。
+        残りは、時のみが表示されるが、年、月、日が変わる場合は、時と合わせて(年)月日表示される。
         (年)月日時を表示させる最初の場所は、変更可能。
 
         Parameters
@@ -17,6 +18,7 @@ class JDayHourFormatter(ticker.Formatter):
             Falseの場合は、時が、06と表示される。Trueの場合は、6時と表示される。
         year : bool (default: True)
             年を表示させるか否か。
+
         """
         self.start_pos = start_pos
         self.ji = ji
@@ -34,6 +36,9 @@ class JDayHourFormatter(ticker.Formatter):
         else:
             year = ""
 
+        if dt.minute != 0 or dt.second != 0:
+            warn("00分以外のところに置かれています。matplotlib.dates.HourLocatorと組み合わせるべきです")
+
         if pos == self.start_pos:
             return dt.strftime(f"{hour}\n{dt.month}月{dt.day}日{year}")
         if dt.hour == 0:
@@ -44,5 +49,4 @@ class JDayHourFormatter(ticker.Formatter):
                     return dt.strftime(f"{hour}\n{dt.month}月{dt.day}日")
             else:
                 return dt.strftime(f"{hour}\n{dt.day}日")
-        else:
-            return dt.strftime(f"{hour}")
+        return dt.strftime(f"{hour}")
